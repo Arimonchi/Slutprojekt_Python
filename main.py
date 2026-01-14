fe5263cad6427ba36b48170bd6b58e092702d174
Arisa <a3135063@gmail.com>
2026-01-14 17:38:08 +0100

    Added dictionary and total_price

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#TEST
+# E-butik systemmet 
+
+
+# "ordbok" för varor
+varor = {
+    1: {"Namn": "Tröja", "pris": 520},
+    2: {"Namn": "Jacka", "pris": 1700},
+    3: {"Namn": "Mössa", "pris": 200},
+    4: {"Namn": "Halsduk", "pris": 300},
+    5: {"Namn": "Strumpor", "pris": 80},
+    6: {"Namn": "Halsband", "pris": 1100},
+    7: {"Namn": "Väska", "pris": 12000},
+    8: {"Namn": "Sneakers", "pris": 1400},
+    9: {"Namn": "T-shirt", "pris": 400},
+    10: {"Namn": "Jeans", "pris": 980},
+}
+
+# total belpop (Börja med 0 kr)
+total_price = 0
